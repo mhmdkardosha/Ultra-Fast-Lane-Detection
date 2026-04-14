121a48a1bbba7345c230a6501a5b110e6dfced30
mhmdkardosha <mohamedkrdsh9@gmail.com>
2026-04-14 10:28:43 +0200

    fix a bug

--- scripts/infer_images_to_video.py
+++ scripts/infer_images_to_video.py
@@ -347,6 +347,7 @@ def main():
 
     img_transforms = transforms.Compose(
         [
+            transforms.ToPILImage(),
             transforms.Resize((288, 800)),
             transforms.ToTensor(),
             transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
